2fd9661e7da3f81abe96895526e73ac06a3eee60
xliangp <dorislx@outlook.com>
2026-05-28 21:13:39 +0000

    Remove IDA-VLM in preparation for subtree import

--- IDA-VLM/prepare_dataset/06_annotated_abcd/convertJson2Jsonl.sh.sh
+++
@@ -1,6 +0,0 @@
-cd /home/xin.liang/code/VLM_gallery_creation/IDA-VLM/prepare_dataset/06_annotated_abcd
-
-for f in benchmarks/*.json; do
-  out="${f%.json}.jsonl"
-  python prepare_jsonl.py --input "$f" --output "$out"
-done
